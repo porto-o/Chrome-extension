faecc2d21ac32e265403e1fbbdc6f30c29d5790f
PORTO <57170000+portocreator@users.noreply.github.com>
2021-02-21 09:21:01 -0600

    s Login Google v2

--- src/views/Popup/Popup.js
+++ src/views/Popup/Popup.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import './Popup.css'
-import Textarea from '../../components/Textarea/Textarea';
-import Time from '../../components/Time/Time';
-import List from '../../components/List/List';
 
 class Popup extends React.Component {
     render()  {
         return (
             <div className="App">
-                <h1 style={{margin:0 + "px"}}>Hello, world!</h1>
-                <List current={[]}/>
+                <h1 style={{margin:0 + "px"}}>Hello, world :))!</h1>
             </div>
         );
     }
